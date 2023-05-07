8f8598dfae78b2a6345c3ac1d228bc63d85ed8a4
Max <117074001+M007AX@users.noreply.github.com>
2023-05-07 12:40:44 +0500

    Create 28.py

---
+++ 28.py
@@ -0,0 +1,13 @@
+alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for p in range(2, 37):
+    for x in alphabet:
+        for y in alphabet:
+            for z in alphabet:
+                first = y + "4" + y
+                second = y + "65"
+                ans = x + z + "33"
+                try:
+                    if int(first, p) + int(second, p) == int(ans, p):
+                        print(int(x + y + z, p))
+                except:
+                    continue
