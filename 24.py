c67c137b82fdda4816b279c2004115c762ead6c2
Max <117074001+M007AX@users.noreply.github.com>
2023-05-10 23:10:40 +0500

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,11 @@
+def prost(x):
+    for j in range(2, int(x**0.5)+1):
+        if x % j == 0:
+            return 0
+    return 1
+
+for n in range(200//2, 400):
+    s = n*2+n*1-1
+    if prost(s):
+        print(n)
+        break
